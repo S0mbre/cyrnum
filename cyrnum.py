fc51243e2ddc8eb0a6eaf416f277f6a06cb31376
s00mbre <s00mbre@gmail.com>
2021-08-25 11:27:44 +1100

    Optimized (natural) Y-alignment of characters; optimized titlo placement; removed xinterval and exception_fontsize from params

--- cyrnum.py
+++ cyrnum.py
@@ -15,9 +15,8 @@ DEFFONT = 'verdana.ttf'
 
 class Cyrnum:
 
-    def __init__(self, fontsize=256, bgcolor='white', color='black', transparent=False, xinterval=4, 
-                 koloda_simple=False, leodr_simple=False, titlo=True, 
-                 draw_exceptions=False, exceptions_fontsize=18):
+    def __init__(self, fontsize=256, bgcolor='white', color='black', transparent=False, 
+                 koloda_simple=False, leodr_simple=False, titlo=True, draw_exceptions=False):
         self.bgcolor = ImageColor.getrgb(bgcolor)
         self.color = ImageColor.getrgb(color)
         if transparent:
@@ -25,16 +24,14 @@ class Cyrnum:
             self.color = (self.color[0], self.color[1], self.color[2], 255)
         self.transparent = transparent
         self.fontsize = fontsize
-        self.xinterval = xinterval
         self.koloda_simple = koloda_simple
         self.leodr_simple = leodr_simple
         self.titlo = titlo
         self.draw_exceptions = draw_exceptions
-        self.exceptions_fontsize = exceptions_fontsize
 
     def _draw_exception(self, message, bgcolor=None, color=None):
         lines = textwrap.wrap(message, width=40)
-        font = ImageFont.truetype(DEFFONT, self.exceptions_fontsize, encoding='unic')
+        font = ImageFont.truetype(DEFFONT, math.ceil(self.fontsize / 4), encoding='unic')
         sizes = [font.getsize(line) for line in lines]
         maxw = max(sizes, key=lambda s: s[0])[0]
         maxh = max(sizes, key=lambda s: s[1])[1]
@@ -63,10 +60,15 @@ class Cyrnum:
             return img.crop(bbox)
         return img
 
-    def _combine_images(self, images, margins_rl=0, margins_tb=0, aligncenter=True):
-        widths, heights = zip(*(i.size for i in images))
+    def _combine_images(self, images, margins_rl=0, margins_tb=0, yoffsets=None):
+        if not images:
+            raise Exception('Нет изображений для склеивания!')
+
+        isdict = isinstance(images[0], dict)
+        widths, heights = zip(*(i['img'].size for i in images)) if isdict else zip(*(i.size for i in images))
+        xinterval = math.ceil(max(widths) * 0.05)
         
-        total_width = sum(widths) + margins_rl*2 + (len(widths) - 1) * self.xinterval
+        total_width = sum(widths) + margins_rl*2 + (len(widths) - 1) * xinterval
         max_height = max(heights)
         total_height = max_height + margins_tb*2
 
@@ -74,10 +76,12 @@ class Cyrnum:
 
         x_offset = margins_rl
         
-        for im in images:
-            yoffset = round((max_height - im.size[1]) / 2) if aligncenter else 0
-            new_im.paste(im, (x_offset, margins_tb + yoffset))
-            x_offset += im.size[0] + self.xinterval
+        for j, im in enumerate(images):
+            img = im['img'] if isdict else im
+            yoffset = im['yoffset'] if isdict else 0
+            if yoffsets: yoffset += yoffsets[j]
+            new_im.paste(img, (x_offset, yoffset))
+            x_offset += img.size[0] + xinterval
             
         return new_im
 
@@ -102,24 +106,29 @@ class Cyrnum:
         img = Image.new('RGBA' if self.transparent else 'RGB', tuple(math.ceil(x * 3) for x in imgsize), self.bgcolor)
         draw = ImageDraw.Draw(img)
         point = (math.floor(imgsize[0] / 1.2), 0)
+
+        outimg = {'img': None, 'yoffset': 0}
+
+        # calc bounding box 
+        tbox = draw.textbbox(point, dig_txt, font=font, **fontparams)
+        tboxw = tbox[2] - tbox[0]
+        tboxh = tbox[3] - tbox[1]
         
         if exp == 3:
             # draw thousand sign
             font2 = ImageFont.truetype(FONTFILE, round(self.fontsize * 0.5), encoding='unic')
             tbox = font2.getbbox(THOUSAND, **fontparams)
-            draw.text((point[0], point[1] + round(self.fontsize / 2)), THOUSAND, fill=self.color, font=font2, **fontparams)
-            point = (point[0] + tbox[2] + 2, point[1])    
+            pt = (point[0], point[1] + round(self.fontsize / 2))
+            draw.text(pt, THOUSAND, fill=self.color, font=font2, **fontparams)
+            point = (point[0] + tbox[2] + 2, point[1])
         
-        draw.text(point, dig_txt, fill=self.color, font=font, **fontparams)
-        # calc bounding box 
-        tbox = draw.textbbox(point, dig_txt, font=font, **fontparams)
-        tboxw = tbox[2] - tbox[0]
-        tboxh = tbox[3] - tbox[1]
+        draw.text(point, dig_txt, fill=self.color, font=font, **fontparams)        
         #draw.rectangle(tbox, outline='red')
             
         if exp < 4:
             # we're done!
-            return self._autocrop(img)    
+            outimg['img'] = self._autocrop(img)
+            return outimg
 
         #print(f'tbox = {tbox}')
         # get longest side
@@ -137,14 +146,15 @@ class Cyrnum:
         #print(f'Center = {center}')
 
         w = math.ceil(self.fontsize / 24)
+        img_src = self._autocrop(img)
+        h_src = img_src.size[1]
 
         if exp == 4:
             # тьма -- кружок
             dif = w * 2
             outerbox2 = [outerbox[0] - dif, outerbox[1] - dif, outerbox[2] + dif, outerbox[3] + dif]
             draw.ellipse(outerbox2, outline=self.color, width=w)
-            return self._autocrop(img)
-            
+                        
         elif exp == 5:
             # легион (неведий) -- кружок из точек
             dif = math.ceil(self.fontsize / 48)
@@ -170,7 +180,7 @@ class Cyrnum:
                             fill=self.color, width=w)
                 else:
                     # в виде запятых
-                    self._draw_txt_rot(img, center[0] + round(r * cos_), center[1] + round(r * sin_), '\u002c', deg + 90)
+                    self._draw_txt_rot(img, center[0] + round(r * cos_), center[1] + round(r * sin_), '\u002c', deg + 270)
             
         elif exp == 7:
             # вран (ворон) -- кружок из крестиков
@@ -244,9 +254,11 @@ class Cyrnum:
             draw.line([x1, y1 + halfstickl, x1 + stickl, y1 + halfstickl], fill=self.color, width=w)
             draw.line([x1 + halfstickl, y1, x1 + halfstickl, y2], fill=self.color, width=w)      
             
-        return self._autocrop(img)
+        outimg['img'] = self._autocrop(img)
+        outimg['yoffset'] = round((h_src - outimg['img'].size[1]) / 2)
+        return outimg
 
-    def _draw_999(self, number, exp=0, interval=3, margins_rl=0, margins_tb=0, **kwargs):
+    def _draw_999(self, number, exp=0, dryrun=False, **kwargs):
         s = str(number)
         if len(s) > 3:
             raise Exception('Функция _draw_999() принимает только 1, 2 и 3-значные числа!')
@@ -254,24 +266,25 @@ class Cyrnum:
         ten = False
         if len(s) > 2:
             hund = int(s[0])
-            images.append((hund, self._draw_digit(hund * 100 if exp < 3 else hund, exp + 2, **kwargs)))
+            num = hund * 100 if exp < 3 else hund
+            images.append({'dig': hund, 'num': num, 'exp': exp + 2, 'char': DIGITS[num], 'img': None if dryrun else self._draw_digit(num, exp + 2, **kwargs)})
             s = s[1:]
         if len(s) > 1:
             tens = int(s[0])
             if tens:
                 ten = (tens == 1)
-                images.append((tens, self._draw_digit(tens * 10 if exp < 3 else tens, exp + 1, **kwargs)))
+                num = tens * 10 if exp < 3 else tens
+                images.append({'dig': ten, 'num': num, 'exp': exp + 1, 'char': DIGITS[num], 'img': None if dryrun else self._draw_digit(num, exp + 1, **kwargs)})
             s = s[1:]
         ones = int(s[0])
         if ones:
-            img_one = (ones, self._draw_digit(ones, exp, **kwargs))
             if exp < 3 and ten:
-                images.insert(1 if len(images) == 2 else 0, img_one)
+                pos = 1 if len(images) == 2 else 0
+                images.insert(pos, {'dig': ones, 'num': ones, 'exp': exp, 'char': DIGITS[ones], 'img': None if dryrun else self._draw_digit(ones, exp, **kwargs)})
             else:
-                images.append(img_one)
+                images.append({'dig': ones, 'num': ones, 'exp': exp, 'char': DIGITS[ones], 'img': None if dryrun else self._draw_digit(ones, exp, **kwargs)})
                 
-        #print([i[0] for i in images])
-        return self._combine_images([i[1] for i in images], interval, margins_rl, margins_tb)
+        return images
 
     def _parse_number(self, number):
         s = str(number)
@@ -281,14 +294,48 @@ class Cyrnum:
             s = s[:-3]
         return l
 
+    def _get_char_offsets(self, text, debug=False, **fontparams):
+        font = ImageFont.truetype(FONTFILE, self.fontsize, encoding='unic')
+        
+        imgsize = font.getbbox(text, **fontparams)
+        m = 10 if debug else 0
+        img = Image.new('RGB', (imgsize[2], imgsize[3] + m * 2), self.bgcolor)
+        draw = ImageDraw.Draw(img)
+        x = 0
+        y = 0
+        if debug:
+            draw.text((x, y + m), text, fill=self.color, font=font, **fontparams)
+            font2 = ImageFont.truetype(DEFFONT, 56, encoding='unic')
+        offsets = []
+        for ch in text:
+            tbox = draw.textbbox((x, y + m), ch, font, **fontparams)
+            offsets.append(tbox[1] - m)
+            if debug:
+                draw.rectangle(tbox, outline='#f00')
+                draw.text((x + (tbox[2] - tbox[0]) // 2, y + 2), str(tbox[1] - m), fill='#f00', font=font2, anchor='mt')
+            x = tbox[2]
+        
+        if debug:
+            img.show()
+
+        return offsets
+
     def draw(self, number):
+        sdif = math.ceil(self.fontsize / 5)
+        ldif = math.ceil(self.fontsize / 3)
+
         try:
             if number < 1 or number > MAXNUMBER:
                 raise Exception('Число должно быть от 1 до 9 999 999 999!')
             chunks = self._parse_number(number)
             l = len(chunks)
-            images = [self._draw_999(num, (l - 1 - i) * 3) for i, num in enumerate(chunks) if num]
-            combimg = self._combine_images(images, self.fontsize//5, self.fontsize//(3 if self.titlo else 5))
+
+            images_all = []
+            for i, num in enumerate(chunks):
+                if num: images_all += self._draw_999(num, (l - 1 - i) * 3)
+
+            yoffsets = [sdif + off for off in self._get_char_offsets(''.join([im['char'] for im in images_all]))]
+            combimg = self._combine_images([im['img'] for im in images_all], sdif, ldif if self.titlo else sdif, yoffsets=yoffsets)
 
         except Exception as err:
             if self.draw_exceptions:
@@ -299,11 +346,11 @@ class Cyrnum:
         # титло
         if self.titlo:
             draw = ImageDraw.Draw(combimg)
-            x1 = self.fontsize//5
-            y1 = x1
-            x2 = combimg.size[0] - x1
+            x1 = sdif
+            y1 = sdif
+            x2 = combimg.size[0] - sdif
             y2 = y1 * 2
-            draw.line([(x1, y2), (x1, y1), (x2, y1), (x2, y1 - x1)], fill=self.color, width=self.fontsize//12, joint='curve')
+            draw.line([(x1, y2), (x1, y1), (x2, y1), (x2, y1 - sdif)], fill=self.color, width=math.ceil(self.fontsize / 12), joint='curve')
         
         return combimg
 
--- cyrnummain.py
+++ cyrnummain.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 # https://en.wikipedia.org/wiki/Cyrillic_numerals
-from cyrnum import Cyrnum
+from cyrnum import Cyrnum, DIGITS
 
 def main():
-    cn = Cyrnum(480, color='blue', transparent=True, draw_exceptions=False)
+    cn = Cyrnum(256, color='blue', transparent=False, draw_exceptions=True, titlo=True)
     
     # randn = cn()
     # print(randn[0])
     # randn[1].show()
 
-    cn[9000000000].show()
+    cn[1234567890].show()
+    # cn.get_char_offsets(''.join(DIGITS.values()))
 
 if __name__ == '__main__':
     main()
