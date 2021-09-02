3615f3d7fdb5212a2c2c8155b3d9c26d662a5d79
s00mbre <s00mbre@gmail.com>
2021-09-03 09:30:31 +1100

    Added Google Analytics (heroku)

--- app.py
+++ app.py
@@ -6,7 +6,34 @@ from dash_bootstrap_components.themes import DARKLY
 meta_tags = [{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.5, minimum-scale=0.5'}]
 ext_css = [dbc.themes.DARKLY] # https://bootswatch.com/darkly/
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, meta_tags=meta_tags, external_stylesheets=ext_css) 
+app = dash.Dash(__name__, suppress_callback_exceptions=True, meta_tags=meta_tags, external_stylesheets=ext_css)
+
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QCSXQ3REHF"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-QCSXQ3REHF');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
 app.title = 'Легионъ имя мне: числа кириллицей'
 app.config.suppress_callback_exceptions = True
 
